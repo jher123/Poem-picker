7ad4a8b5c7f4b2de034d9345ee7f402587d4f765
Joanna <Joanna.k.herman@gmail.com>
2022-04-20 22:08:39 +0200

    2- set up routes with express and pug page templates

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 app.get('/', (req, res) => {
-  res.send('<em>Hello World!</em>')
+  res.render('pages/home', {
+    meta: {
+      data: {
+        title: 'Floema',
+        description: 'Metadata description'
+      }
+    }
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('pages/about', {
+    meta: {
+      data: {
+        title: 'Floema',
+        description: 'Metadata description'
+      }
+    }
+  })
+})
+
+app.get('/detail/:id', (req, res) => {
+  res.render('pages/detail', {
+    meta: {
+      data: {
+        title: 'Floema',
+        description: 'Metadata description'
+      }
+    }
+  })
+})
+
+app.get('/collections', (req, res) => {
+  res.render('pages/collections', {
+    meta: {
+      data: {
+        title: 'Floema',
+        description: 'Metadata description'
+      }
+    }
+  })
 })
 
 app.listen(port, () => {
