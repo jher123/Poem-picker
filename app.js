404d3b4305d0023f287320bd3bcd5c3d664cd9b6
Joanna <Joanna.k.herman@gmail.com>
2022-04-25 17:24:04 +0200

    fix Prismic issues by upgrading Prismic packages and refactoring app.js

--- app.js
+++ app.js
@@ -1,30 +1,51 @@
+/* eslint-disable no-unused-vars */
 require('dotenv').config()
 
-const express = require('express')
-const app = express()
+const fetch = require('node-fetch')
+const logger = require('morgan')
 const path = require('path')
-const port = 3000
+const express = require('express')
+const errorHandler = require('errorhandler')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+const app = express()
+const port = process.env.PORT || 8004
 
 const Prismic = require('@prismicio/client')
-const PrismicDOM = require('prismic-dom')
-// const find = require('lodash/find')
+const PrismicH = require('@prismicio/helpers')
+const { application } = require('express')
+const UAParser = require('ua-parser-js')
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(errorHandler())
+app.use(methodOverride())
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Initialize the prismic.io api
-const initApi = req => {
-  return Prismic.getGraphQLEndpoint(process.env.PRISMIC_ENDPOINT, {
+const initApi = (req) => {
+  return Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-    req
+    req,
+    fetch
   })
 }
 
 // Link Resolver
-const handleLinkResolver = doc => {
-  // Define the url depending on the document type
-  //   if (doc.type === 'page') {
-  //     return '/page/' + doc.uid;
-  //   } else if (doc.type === 'blog_post') {
-  //     return '/blog/' + doc.uid;
-  //   }
+const HandleLinkResolver = (doc) => {
+  if (doc.type === 'product') {
+    return `/detail/${doc.slug}`
+  }
+
+  if (doc.type === 'collections') {
+    return '/collections'
+  }
+
+  if (doc.type === 'about') {
+    return '/about'
+  }
 
   // Default to homepage
   return '/'
@@ -32,94 +53,146 @@ const handleLinkResolver = doc => {
 
 // Middleware to inject prismic context
 app.use((req, res, next) => {
-  res.locals.ctx = {
-    endpoint: process.env.PRISMIC_ENDPOINT,
-    linkResolver: handleLinkResolver
-  }
+  const ua = UAParser(req.headers['user-agent'])
 
-  res.locals.PrismicDOM = PrismicDOM
+  res.locals.isDesktop = ua.device.type === undefined
+  res.locals.isPhone = ua.device.type === 'mobile'
+  res.locals.isTablet = ua.device.type === 'tablet'
+
+  res.locals.Link = HandleLinkResolver
+  res.locals.PrismicH = PrismicH
+  res.locals.Numbers = (index) => {
+    return index === 0
+      ? 'One'
+      : index === 1
+        ? 'Two'
+        : index === 2
+          ? 'Three'
+          : index === 3
+            ? 'Four'
+            : ''
+  }
 
   next()
 })
 
-// app.use((request, response, next) => {
-//   const accessToken = process.env.PRISMIC_ACCESS_TOKEN
-//   const endpoint = process.env.PRISMIC_ENDPOINT
-
-//   response.locals.PrismicDOM = PrismicDOM
-
-//   Prismic.api(endpoint, {
-//     accessToken,
-//     request
-//   }).then(api => {
-//     request.prismic = { api }
-
-//     next()
-//   }).catch(error => {
-//     next(error.message)
-//   })
-// })
-
-app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+app.locals.basedir = app.get('views')
 
-app.get('/', (req, res) => {
-  res.render('pages/home', {
-    meta: {
-      data: {
-        title: 'Floema',
-        description: 'Metadata description'
-      }
+const handleRequest = async (api) => {
+  const [meta, preloader, navigation, home, about, { results: collections }] =
+    await Promise.all([
+      api.getSingle('meta'),
+      api.getSingle('preloader'),
+      api.getSingle('navigation'),
+      api.getSingle('home'),
+      api.getSingle('about'),
+      api.query(Prismic.Predicates.at('document.type', 'collection'), {
+        fetchLinks: 'product.image'
+      })
+    ])
+
+  //   console.log(about, home, collections);
+
+  const assets = []
+
+  home.data.gallery.forEach((item) => {
+    assets.push(item.image.url)
+  })
+
+  about.data.gallery.forEach((item) => {
+    assets.push(item.image.url)
+  })
+
+  about.data.body.forEach((section) => {
+    if (section.slice_type === 'gallery') {
+      section.items.forEach((item) => {
+        assets.push(item.image.url)
+      })
     }
   })
-})
 
-app.get('/about', (req, res) => {
-  initApi(req).then(api => {
-    api.query(Prismic.predicate.any('document.type', ['meta', 'About'])).then(response => {
-      const { results } = response
-      const [about, meta] = results
-
-      res.render('pages/about', {
-        about,
-        meta
-      })
+  collections.forEach((collection) => {
+    collection.data.products.forEach((item) => {
+      assets.push(item.products_product.data.image.url)
     })
   })
-})
 
-//   res.render('pages/about', {
-//     meta: {
-//       data: {
-//         title: 'Floema',
-//         description: 'Metadata description'
-//       }
-//     }
-//   })
-// })
+  console.log(assets)
 
-app.get('/detail/:id', (req, res) => {
-  res.render('pages/detail', {
-    meta: {
-      data: {
-        title: 'Floema',
-        description: 'Metadata description'
-      }
-    }
+  return {
+    assets,
+    meta,
+    home,
+    collections,
+    about,
+    navigation,
+    preloader
+  }
+}
+
+app.get('/', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  res.render('pages/home', {
+    ...defaults
   })
 })
 
-app.get('/collections', (req, res) => {
+app.get('/about', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  res.render('pages/about', {
+    ...defaults
+  })
+})
+
+app.get('/collections', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
   res.render('pages/collections', {
-    meta: {
-      data: {
-        title: 'Floema',
-        description: 'Metadata description'
-      }
-    }
+    ...defaults
+  })
+})
+
+// app.get('/detail/:uid', async (req, res) => {
+//   const api = await initApi(req);
+//   const defaults = await handleRequest(api);
+
+//   const product = await api.getByUID('product', req.params.uid, {
+//     fetchLinks: 'collection.title',
+//   });
+
+//   if (product) {
+//     res.render('pages/detail', {
+//       product,
+//       ...defaults,
+//     });
+//   } else {
+//     res.status(404).send('Page not found');
+//   }
+// });
+
+app.get('/detail/:uid', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  const product = await api.getByUID('product', req.params.uid, {
+    fetchLinks: 'collection.title'
+  })
+
+  res.render('pages/detail', {
+    ...defaults,
+    product
   })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
+
+// "start": "concurrently --kill-others \"npm run backend:development\" \"npm run frontend:development\""
